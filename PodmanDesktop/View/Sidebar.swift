47f15c205ba09a870225d4c0de8f51194f916c11
LUO Sheng <luosheng1986@gmail.com>
2021-09-07 10:01:18 +0800

    Add sidebar

---
+++ PodmanDesktop/View/Sidebar.swift
@@ -0,0 +1,20 @@
+//
+//  Sidebar.swift
+//  PodmanDesktop
+//
+//  Created by Luo Sheng on 2021/9/7.
+//
+
+import SwiftUI
+
+struct Sidebar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Sidebar_Previews: PreviewProvider {
+    static var previews: some View {
+        Sidebar()
+    }
+}
