df636ad61e2a2166b5987dcb9e36981a905a36b5
LUO Sheng <luosheng1986@gmail.com>
2021-09-07 16:27:25 +0800

    Add event model

---
+++ PodmanDesktop/Model/Event.swift
@@ -0,0 +1,16 @@
+//
+//  Event.swift
+//  PodmanDesktop
+//
+//  Created by Luo Sheng on 2021/9/7.
+//
+
+import Foundation
+
+struct Event {
+    var name: String
+    var status: String
+    var time: String
+    var type: String
+    var attributes: [String:AnyObject]
+}
