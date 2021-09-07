cc853d9750e24b43944bf1d4933736396b0b2592
LUO Sheng <luosheng1986@gmail.com>
2021-09-07 10:11:56 +0800

    Add container view

---
+++ PodmanDesktop/View/ContainerView.swift
@@ -0,0 +1,20 @@
+//
+//  ContainerView.swift
+//  PodmanDesktop
+//
+//  Created by Luo Sheng on 2021/9/7.
+//
+
+import SwiftUI
+
+struct ContainerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContainerView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContainerView()
+    }
+}
--- PodmanDesktop/View/Sidebar.swift
+++ PodmanDesktop/View/Sidebar.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct Sidebar: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List {
+                NavigationLink(destination: ContainerView()) {
+                    Text("Containers / Apps")
+                }
+            }
+        }
     }
 }
 
