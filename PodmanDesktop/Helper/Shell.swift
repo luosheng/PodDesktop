6b364a0d73d9c8a1e8f1a3885c3a0fbf73e0ec56
LUO Sheng <luosheng1986@gmail.com>
2021-09-07 11:09:29 +0800

    Add shell helper

---
+++ PodmanDesktop/Helper/Shell.swift
@@ -0,0 +1,22 @@
+//
+//  Shell.swift
+//  PodmanDesktop
+//
+//  Created by Luo Sheng on 2021/9/7.
+//
+
+import Foundation
+
+func shell(_ launchPath: String, _ arguments: [String]) -> String? {
+    let task = Process()
+    task.launchPath = launchPath
+    task.arguments = arguments
+    
+    let pipe = Pipe()
+    task.standardOutput = pipe
+    task.launch()
+    
+    let data = pipe.fileHandleForReading.readDataToEndOfFile()
+    let output = String(data: data, encoding: .utf8)
+    return output
+}
