ff91349f610733f78c7aebc01e7a1e319e804fa6
LUO Sheng <luosheng1986@gmail.com>
2021-09-07 09:56:53 +0800

    Initial Commit

---
+++ PodmanDesktop/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PodmanDesktop
+//
+//  Created by Luo Sheng on 2021/9/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PodmanDesktop/PodmanDesktopApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodmanDesktopApp.swift
+//  PodmanDesktop
+//
+//  Created by Luo Sheng on 2021/9/7.
+//
+
+import SwiftUI
+
+@main
+struct PodmanDesktopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
