6c509cbddbbfd968069cd7a146dccf54dc498b2f
LUO Sheng <luosheng1986@gmail.com>
2021-09-10 12:20:01 +0800

    Parse lima event

--- PodmanDesktop/Model/Event.swift
+++ PodmanDesktop/Model/Event.swift
@@ -8,17 +8,33 @@
 import Foundation
 
 struct Event: Codable {
-    var name: String
-    var status: String
-    var time: String
-    var type: String
-    var attributes: [String:String]
+    var containerId: String
+    var processId: Int32
+    var type: String!
     
     enum CodingKeys: String, CodingKey {
-        case name = "Name"
-        case status = "Status"
-        case time = "Time"
-        case type = "Type"
-        case attributes = "Attributes"
+        case containerId = "container_id"
+        case processId = "pid"
+    }
+    
+    static func parse(output: String) throws -> Event {
+        let pattern = #"(?<type>\/\S*)\s*(?<json>\{.*\})"#
+        let regex = try NSRegularExpression(pattern: pattern, options: [])
+        let range = NSRange(output.startIndex..<output.endIndex, in: output)
+        guard let match = regex.firstMatch(in: output, options: [], range: range) else {
+            fatalError("Can not decode event log")
+        }
+        
+        guard let typeRange = Range(match.range(withName: "type"), in: output),
+              let jsonRange = Range(match.range(withName: "json"), in: output) else {
+            fatalError("Can not decode event log")
+        }
+        let type = output[typeRange]
+        let json = output[jsonRange]
+        
+        var event = decode(Event.self, from: String(json))
+        event.type = String(type)
+        
+        return event
     }
 }
