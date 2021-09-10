f7d99eb34896906ac826d7bb58777c58cb8394e0
LUO Sheng <luosheng1986@gmail.com>
2021-09-10 14:01:57 +0800

    Put header inside list

--- PodDesktop/View/OCIImageView.swift
+++ PodDesktop/View/OCIImageView.swift
@@ -14,43 +14,44 @@ struct OCIImageView: View {
     @State var imageToBuild: String = ""
     
     var body: some View {
-        VStack {
-            List() {
-                ForEach(imageStore.images) { image in
-                    OCIImageItem(image: image)
+        List() {
+            VStack(alignment: .leading) {
+                Text("Image Acquistition")
+                    .font(.title)
+                HStack {
+                    TextField("Name of image to pull", text: $imageToPull)
+                    Button("Pull Image") {
+                        print(imageToPull)
+                    }
+                }
+                HStack {
+                    TextField("Name of image to build", text: $imageToBuild)
+                    Button("Build Image...") {
+                        self.browseFile()
+                    }
                 }
             }
-            Text("Image Acquistition")
-                .font(.title)
-            HStack {
-                TextField("Name of image to pull", text: $imageToPull)
-                Button("Pull Image") {
-                    print(imageToPull)
-                }
-            }
-            HStack {
-                TextField("Name of image to build", text: $imageToBuild)
-                Button("Build Image...") {
-                    self.browseFile()
-                }
+            .padding()
+            .background(Color(red: 237.0 / 255.0, green: 242.0 / 255.0, blue: 247.0 / 255.0))
+            ForEach(imageStore.images) { image in
+                OCIImageItem(image: image)
             }
         }
         .onAppear(perform: {
             imageStore.fetch()
         })
-        .padding()
     }
     
     private func browseFile() {
         let dialog = NSOpenPanel()
-            
+        
         dialog.title                   = "Choose a Docker file"
         dialog.showsResizeIndicator    = true
         dialog.showsHiddenFiles        = false
         dialog.canChooseDirectories    = false
         dialog.canCreateDirectories    = false
         dialog.allowsMultipleSelection = false
-
+        
         if (dialog.runModal() == NSApplication.ModalResponse.OK) {
             let result = dialog.url // Pathname of the file
             if let path = result?.path {
